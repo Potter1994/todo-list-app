ca85cc7e8e51b26bc5f2120dfc6ee8f36d370b3f
Potter <sky753159852@gmail.com>
2025-03-14 02:18:48 +0800

    Updates

--- utils/classnames.ts
+++
@@ -1,3 +0,0 @@
-export const buttonBase = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer active:bg-blue-500'
-
-export const checkboxBase = 'w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
